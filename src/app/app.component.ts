90962b67b683be36d8cdd157129748a9567c53c7
Shaheer cp <shaheercp890@gmail.com>
2025-03-01 21:47:03 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone : true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myProject';
+  public task = '';
+  public list:string[]=[];
+  public editIndex: number | null = null;
+  public editedTask:string = ''
+
+
+  ngOnInit(){
+    const storedList = localStorage.getItem('Task');
+    this.list = storedList ? JSON.parse(storedList):[]
+  }
+  addTask(){
+    if(this.task.trim()){
+      this.list.push(this.task.trim());
+      this.storeTolocal()
+    }
+  }
+  removeTask(index:number){
+    this.list.splice(index,1)
+    this.storeTolocal()
+  }
+  editTask(index:number,task:string){
+    this.editIndex =index;
+    this.editedTask = task
+  }
+
+  storeTolocal(){
+    localStorage.setItem('Task',JSON.stringify(this.list));
+  }
+
+  
 }
